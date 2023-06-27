e94e58de2e4707b99a4c3f868563444069a4c96b
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-27 09:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+
+function createRandomDelayPromise() {
+  const min = 1000; // 1 second
+  const max = 5000; // 5 seconds
+  const delay = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(delay);
+    }, delay);
+  });
+}
+
+for (let i = 0; i < 5; i++) {
+  promises.push(createRandomDelayPromise());
+}
+
+Promise.any(promises)
+  .then((result) => {
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = `Resolved after ${result} milliseconds.`;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
