53d3bfe58cc9594ae8043eb50619e5f64dd3f980
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-01 16:01:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.promises = [];
 
-
 function createRandomDelayPromise() {
   const min = 1000; // 1 second
   const max = 5000; // 5 seconds
@@ -17,11 +16,14 @@ for (let i = 0; i < 5; i++) {
   promises.push(createRandomDelayPromise());
 }
 
-Promise.any(promises)
-  .then((result) => {
-    const outputElement = document.getElementById("output");
-    outputElement.textContent = `Resolved after ${result} milliseconds.`;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  Promise.any(promises)
+    .then((result) => {
+      const outputElement = document.getElementById("output");
+      outputElement.textContent = `Resolved after ${result} milliseconds.`;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
